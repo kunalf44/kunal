4c8c76ca9b243ce32f6c8160e64324be56162962
kunalf44 <86444651+kunalf44@users.noreply.github.com>
2021-07-12 16:12:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+        mapId: '56d1e42afa8700a5 '
+      });
+    }
